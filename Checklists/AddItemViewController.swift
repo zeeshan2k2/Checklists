feca22a9ca8c51fdf634ff861cd29de6cb272c66
Zeeshan <zeeshan.12.zw@gmail.com>
2024-04-09 22:44:57 +0500

    part 4 done

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,58 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Zeeshan Waheed on 09/04/2024.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        navigationItem.largeTitleDisplayMode = .never
+        
+        textField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func cancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        print("Contents of the text field: \(textField.text!)")
+        navigationController?.popViewController(animated: true)
+        
+    }
+
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+    
+
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -15,16 +15,20 @@ class ChecklistViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let item1 = ChecklistItem()
         item1.text = "Walk the dog"
         items.append(item1)
         
         let item2 = ChecklistItem()
         item2.text = "Brush my teeth"
+        item2.checked = true
         items.append(item2)
         
         let item3 = ChecklistItem()
         item3.text = "Learn IOS development"
+        item3.checked = true
         items.append(item3)
         
         let item4 = ChecklistItem()
@@ -33,12 +37,25 @@ class ChecklistViewController: UITableViewController {
         
         let item5 = ChecklistItem()
         item5.text = "Eat ice cream"
+        item5.checked = true
         items.append(item5)
         
     }
     
     @IBOutlet var textLabel: UILabel!
     
+    
+    @IBAction func addItem(_ sender: Any) {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         
 //        let item = items[indexPath.row]
@@ -86,6 +103,13 @@ class ChecklistViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
     }
     
 }
