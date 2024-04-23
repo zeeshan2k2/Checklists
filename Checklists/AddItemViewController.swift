9843f4a8e32280a3b9fac3c1f38b5f07ccc5368d
Zeeshan <zeeshan.12.zw@gmail.com>
2024-04-23 16:05:31 +0500

    implmenting protocol

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -1,50 +1,65 @@
 import UIKit
 
+protocol AddItemViewControllerDelegate: AnyObject {
+    
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+    
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
+    
+}
+
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
-  @IBOutlet weak var doneBarButton: UIBarButtonItem!
-  @IBOutlet weak var textField: UITextField!
-    
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    textField.becomeFirstResponder()
-  }
-  
-  // MARK:- Actions
-  @IBAction func cancel() {
-    navigationController?.popViewController(animated: true)
-  }
-  
-  @IBAction func done() {
-    print("Contents of the text field: \(textField.text!)")
-    
-    navigationController?.popViewController(animated: true)
-  }
-  
-  // MARK:- Table View Delegates
-  override func tableView(_ tableView: UITableView,
-                          willSelectRowAt indexPath: IndexPath)
+        @IBOutlet weak var doneBarButton: UIBarButtonItem!
+        @IBOutlet weak var textField: UITextField!
+        
+        weak var delegate: AddItemViewControllerDelegate?
+
+        override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
+    // MARK:- Actions
+    @IBAction func cancel() {
+    //    navigationController?.popViewController(animated: true)
+        delegate?.addItemViewControllerDidCancel(self)
+    }
+
+    @IBAction func done() {
+    //    print("Contents of the text field: \(textField.text!)")
+    //
+    //    navigationController?.popViewController(animated: true)
+        let item = ChecklistItem()
+        item.text = textField.text!
+        
+        delegate?.addItemViewController(self, didFinishAdding: item)
+    }
+
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                      willSelectRowAt indexPath: IndexPath)
     -> IndexPath? {
-      return nil
-  }
-  
-  // MARK:- Text Field Delegates
-  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    
-    let oldText = textField.text!
-    let stringRange = Range(range, in:oldText)!
-    let newText = oldText.replacingCharacters(in: stringRange,
+    return nil
+    }
+
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
                                               with: string)
-    doneBarButton.isEnabled = !newText.isEmpty
-    return true
-  }
-  
-  func textFieldShouldClear(_ textField: UITextField) -> Bool {
-    doneBarButton.isEnabled = false
-    return true
-  }
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
 }
 
