bef3564827d4a0f78d10f2704f141a0efc4cd6cb
Zeeshan <zeeshan.12.zw@gmail.com>
2024-04-23 16:13:50 +0500

    added button segue

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddItem" {
+            let controller = segue.destination as! AddItemViewController
+            controller.delegate = self
+        }
+    }
     
     var items = [ChecklistItem]()
     
