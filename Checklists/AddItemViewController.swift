92a6603c57c9aba3dff462a7ab910df8ec312d58
Zeeshan <zeeshan.12.zw@gmail.com>
2024-04-24 22:29:38 +0500

    completed

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -3,20 +3,12 @@ import UIKit
 protocol AddItemViewControllerDelegate: AnyObject {
     
     func addItemViewControllerDidCancel(_ controller: AddItemViewController)
-    
-    //    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
-    
+
     func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
     
     func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
     
 }
-
-//protocol AddItemViewControllerDelegate: AnyObject {
-//  func addItemViewControllerDidCancel(_ controller: AddItemViewController)
-//  func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
-//  func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
-//}
 
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
         @IBOutlet weak var doneBarButton: UIBarButtonItem!
@@ -41,21 +33,12 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
         textField.becomeFirstResponder()
     }
 
-    // MARK:- Actions
+
     @IBAction func cancel() {
-    //    navigationController?.popViewController(animated: true)
         delegate?.addItemViewControllerDidCancel(self)
     }
 
     @IBAction func done() {
-    //    print("Contents of the text field: \(textField.text!)")
-        
-    //    navigationController?.popViewController(animated: true)
-//        let item = ChecklistItem()
-//        item.text = textField.text!
-//        
-//        delegate?.addItemViewController(self, didFinishAdding: item)
-        
         if let item = itemToEdit {
             item.text = textField.text!
             delegate?.addItemViewController(self, didFinishEditing: item)
@@ -66,14 +49,12 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
         }
     }
 
-    // MARK:- Table View Delegates
     override func tableView(_ tableView: UITableView,
                       willSelectRowAt indexPath: IndexPath)
     -> IndexPath? {
     return nil
     }
 
-    // MARK:- Text Field Delegates
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
         let oldText = textField.text!
